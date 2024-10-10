1d12138c20e98464c166447bef984a61c92ec2b2
pavanvasoya <pavan.sarvadhi@gmail.com>
2024-10-10 16:29:31 +0530

    add winning-combinations

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
 
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBorad = initialGameBoard;
 
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
+export default function GameBoard({ onSelectSquare, board }) {
 
-    gameBorad[row][col] = player;
-  }
 
   // const [gameBorad, SetGameBoard] = useState(initialGameBoard);
 
@@ -29,12 +18,15 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
   return (
     <ol id="game-board">
-      {gameBorad.map((row, rowIndex) => (
+      {borad.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
